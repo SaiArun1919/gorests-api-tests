7468d7110776c078cd7481455edf8ebc029202f3
Arun B Kumar <arun.b.aux@go-jek.com>
2022-08-10 20:57:51 +0530

    Local commit

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,11 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+    @Test
+    public void calulatorAdd(){
+        System.out.println(2+3);
+        Assert.assertEquals(1,1);
+    }
+}
